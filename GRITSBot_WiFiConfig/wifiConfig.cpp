27cc730426bf0024df0a7c91331700a84d7f4ed4
Siddharth Mayya <siddharth.mayya@gatech.edu>
2018-09-07 11:07:15 -0400

    change in wifi config

--- GRITSBot_WiFiConfig/wifiConfig.cpp
+++ GRITSBot_WiFiConfig/wifiConfig.cpp
@@ -2,6 +2,4 @@
 
 extern const char* wifiConfig::wifiSSID      = "robotarium_setup_ap";
 extern const char* wifiConfig::wifiPassword  = "robotarium_setup";
-//extern const char* wifiConfig::wifiSSID      = "GTother";
-//extern const char* wifiConfig::wifiPassword  = "GeorgeP@1927";
 extern const char* wifiConfig::wifiBroadcast = "192.168.18.255";
